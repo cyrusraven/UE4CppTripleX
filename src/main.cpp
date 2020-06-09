f46a04531fe6e21fcd49da8e25216da35fd8af97
cyrusraven <dan.amaralsilva@gmail.com>
2020-06-09 14:25:16 -0300

    finished the project

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <random>
 
 // function declared before being used
-void PlayGameAtDifficulty(int Difficulty)
+bool PlayGame(int Difficulty)
 {
     // declaring multiple values for this round
     
@@ -21,9 +21,9 @@ void PlayGameAtDifficulty(int Difficulty)
      * where every word inside the name of the variables start with
      * an uppercase letter
      */ 
-    const int CodeA = 1;
-    const int CodeB = 7;
-    const int CodeC = 15;
+    const int CodeA = (rand() % Difficulty) + 1;
+    const int CodeB = (rand() % Difficulty) + 1;
+    const int CodeC = (rand() % Difficulty) + 1;
 
     /*
      * in a multiline comment
@@ -33,12 +33,12 @@ void PlayGameAtDifficulty(int Difficulty)
     const int CodeProduct = CodeA * CodeB * CodeC;
 
     // expression statements
-    std::cout << "difficulty: " << Difficulty << std::endl; // quick check
-    std::cout << "just another day in the BBMP Space Station, unless..." << std::endl;
+    std::cout << "Day: " << Difficulty << std::endl; // quick check
+    std::cout << "Just another day in the BBMP Space Station, unless..." << std::endl;
     std::cout << "OH NO! your air pressure system is malfunctioning (again)!" << std::endl;
     std::cout << "\n";
-
-    std::cout << "+ The first value: " << CodeA << std::endl << "The second value: " << CodeB << std::endl << "The third value: " << CodeC << std::endl;
+    std::cout << "You look at the screen and see the following messages:" << std::endl;
+    std::cout << "+ There are three numbers missing on the system..." << std::endl;
     std::cout << "+ The sum is: " << CodeSum << "\n";
     std::cout << "+ The product is: " << CodeProduct << "\n";
     
@@ -47,24 +47,33 @@ void PlayGameAtDifficulty(int Difficulty)
     // std::cin >> PlayerGuess;
 
     int GuessA, GuessB, GuessC;
-
-    std::cout << "First Guess> ";
+    std::cout << "+ Please input the missing numbers: " << std::endl;
+    std::cout << "+ First Guess> ";
     std::cin >> GuessA;
     std::cout << std::endl;
-    std::cout << "Second Guess> ";
+    std::cout << "+ Second Guess> ";
     std::cin >> GuessB;
     std::cout << std::endl;
-    std::cout << "Third Guess> ";
+    std::cout << "+ Third Guess> ";
     std::cin >> GuessC;
     std::cout << std::endl;
 
-    int CodeSum = GuessA + GuessB + GuessC;
-    int CodeProduct = GuessA * GuessB * GuessC;
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
 
-    std::cout << "You entered: " << GuessA << GuessB << GuessC;
+    std::cout << "You entered: " << GuessA << " X " << GuessB << " X " << GuessC;
     std::cout << std::endl;
 
-    return ; // return statement
+    if(CodeSum == GuessSum && CodeProduct == GuessProduct)
+    {
+        std::cout << "You succeded in fixing the air system. You live to see another day!";
+        std::cout << std::endl;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 // main function in the bottom
@@ -73,22 +82,27 @@ void PlayGameAtDifficulty(int Difficulty)
 
 int main()
 {
+    srand(time(NULL)); // creates a new seed based on the time of the day
+    std::cin.clear();
+    std::cout << std::system("clear") << ">" << std::endl;;
+    
     std::cout << "Hi! You've been assigned to work (alone) on the BBMP Space Station!" << std::endl;
     std::cout << "Unfortunately, the space station has a serious problem with the air pressure system..." << std::endl;
-    int Difficulty = 2;
-    int MaxDifficulty = 10;
-    while(Difficulty <= MaxDifficulty)
+    int LevelDifficulty = 1;
+    const int MaxDifficulty = 6;
+    bool bLevelComplete = true;
+    while (LevelDifficulty <= MaxDifficulty && bLevelComplete) // loops while all levels are completed or until the player fails to complete a level
     {
-        PlayGameAtDifficulty(Difficulty);
+        bLevelComplete = PlayGame(LevelDifficulty);
 
         std::cin.clear();  // clears the failbit
         std::cin.ignore(); // discards the buffer
 
         // somewhat waits for the user input
         // increases difficulty
-        ++Difficulty;
+        ++LevelDifficulty;
     }
-    if(Difficulty >= MaxDifficulty)
+    if(LevelDifficulty >= MaxDifficulty && bLevelComplete)
     {
         std::cout << "YOU WON!" << std::endl;
         std::cout << "You've survived until the day you returned to earth!" << std::endl;
@@ -97,9 +111,7 @@ int main()
     else
     {
         std::cout << "GAME OVER!" << std::endl;
-        std::default_random_engine Generator;
-        std::uniform_int_distribution<int> Distribution(1, 3);
-        int WhyDidYouDie = Distribution(Generator);
+        int WhyDidYouDie = (rand() % 3) + 1;
         switch (WhyDidYouDie)
         {
         case 1:
